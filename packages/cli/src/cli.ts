d28c1383179898e51cd94db2112b3d0dc73aa1c0
YTeyddie22 <teyddie@gmail.com>
2025-07-20 03:50:32 +0300

    feat: main cli program

---
+++ packages/cli/src/cli.ts
@@ -0,0 +1,42 @@
+import { Effect } from "effect";
+
+import { intro, select, isCancel, outro, log } from "@clack/prompts";
+import * as interactives from "./interactives";
+import { Console } from "effect";
+
+const program = Effect.gen(function* (_) {
+	const args = process.argv.slice(2);
+
+	if (args.length === 0) {
+		yield* _(Effect.sync(() => intro("`Welcome to Nurser's CLI")));
+
+		const selectedCommand = yield* _(
+			Effect.tryPromise(() =>
+				select({
+					message: `Hello ${process.env.USER}, What do we do?`,
+					options: Object.values(interactives).map((interactive) => ({
+						label: interactive.description,
+						value: interactive.id,
+					})),
+					maxItems: 5,
+				})
+			)
+		);
+
+		if (isCancel(selectedCommand)) {
+			yield* _(Effect.sync(() => outro(`No worries, come anytime`)));
+			return yield* _(Effect.sync(() => process.exit(0)));
+		}
+
+		return yield* _(runInteractive(selectedCommand as string));
+	} else {
+		yield* _(Effect.sync(() => `Nurser CLI`));
+		const firstArg = args[0];
+
+		if (["help", "-h", "--help"].includes(firstArg)) {
+			return yield* _(printHelp);
+		}
+
+		return yield* _(runInteractive(firstArg));
+	}
+});
